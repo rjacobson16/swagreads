f6670cbac00129efe401d4834a8bed11b61ca071
Rachel Jacobson <rmjacobson1@gmail.com>
2017-09-20 22:46:49 -0700

    bootstrapped current user: functional auth

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -5,13 +5,16 @@ import Root from './components/root';
 import {login, signup, logout} from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
+  let store;
+   if (window.currentUser) {
+     const preloadedState = { session: { currentUser: window.currentUser } };
+     store = configureStore(preloadedState);
+     delete window.currentUser;
+   } else {
+     store = configureStore();
+   }
 
-
-
-  window.store = store;
   window.getState = store.getState;
-  window.dispatch = store.dispatch;
 
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
