9cfda21ad520498a1c4438ff90e31706b85f8e48
Rachel Jacobson <rmjacobson1@gmail.com>
2019-07-31 14:14:33 -0700

    add grid header

--- frontend/components/home_page/book_list/book_list.jsx
+++ frontend/components/home_page/book_list/book_list.jsx
@@ -4,11 +4,20 @@ import "./book_list.scss";
 
 function BookList({ books }) {
   return (
-    <ul className="book-list">
-      {books.map(book => (
-        <BookListItem key={book.id} book={book} />
-      ))}
-    </ul>
+    <section className="book-list-container">
+      <ul className="book-list-header">
+        <li>cover</li>
+        <li>title</li>
+        <li>author</li>
+        <li>avg rating</li>
+        <li>rating</li>
+      </ul>
+      <ul className="book-list">
+        {books.map(book => (
+          <BookListItem key={book.id} book={book} />
+        ))}
+      </ul>
+    </section>
   );
 }
 
